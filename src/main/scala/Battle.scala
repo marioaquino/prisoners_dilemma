afb6769b0834a0e6e51de6a18d962e6505decd95
Jessica Kerr <jessitron@gmail.com>
2013-09-03 21:11:57 -0500

    Jess adds a battle

---
+++ src/main/scala/Battle.scala
@@ -0,0 +1,39 @@
+// When two players, are matched against each other, they play a series of rounds.
+import Tournament._
+
+case class BattleResult(aScore: Score, bScore: Score)
+
+trait Rules {
+   def score(plays: (Play, Play)): (Score, Score) = plays match {
+     case (Cooperate, Cooperate) => (bothCooperate, bothCooperate)
+     case (Cooperate, Defect) => (loneCooperator, loneDefector)
+     case (Defect, Cooperate) => (loneDefector, loneCooperator)
+     case (Defect, Defect) => (bothDefect, bothDefect)
+   }
+
+   def bothCooperate: Score
+   def bothDefect: Score
+   def loneDefector: Score
+   def loneCooperator: Score
+}
+
+trait OneSetOfRules extends Rules {
+  val bothCooperate = 300
+  val bothDefect = -300
+  val loneCooperator = -500
+  val loneDefector = 500
+}
+
+class Battle(numberOfRounds: Int) {
+   self: Rules =>
+
+   def pit(a: Player, b: Player): BattleResult = {
+      val resultOfOne = score(singleRound(a,b))
+      BattleResult(resultOfOne._1, resultOfOne._2)
+   }
+
+   private def singleRound(a: Player, b:Player): (Play, Play) =
+     (a.play(b.name), b.play(a.name))
+
+
+}
--- src/main/scala/Player.scala
+++ src/main/scala/Player.scala
@@ -1,10 +1,19 @@
 object Tournament {
   type History = Seq[Round]
   type PlayerId = String
+  type Score = Int
+}
+import Tournament._
+
+abstract trait Strategy {
+   def decide(h: History): Play
 }
 
-import Tournament._
-class Player(name: PlayerId) {
+trait AlwaysCooperate extends Strategy {
+   def decide(h:History) = Cooperate
+}
+
+class Player(val name: PlayerId) {
 
    self: Strategy =>
 
@@ -12,7 +21,7 @@ class Player(name: PlayerId) {
 
    def play(opponent: PlayerId): Play = decide(history.getOrElse(opponent, Seq()))
 
-   def addToHistory(opponent: PlayerId, round: Round) = history = history + (opponent -> Seq(round))
+   def addToHistory(opponent: PlayerId, round: Round) = history = history + ((opponent, round +: history.getOrElse(opponent, Seq())))
 }
 
 
