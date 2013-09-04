930e1a03111bec3c21278218e4299523a8e0d0fd
Mario Aquino <mario.e.aquino@gmail.com>
2013-09-03 22:49:41 -0500

    Add many strategies

--- src/main/scala/Player.scala
+++ src/main/scala/Player.scala
@@ -12,7 +12,68 @@ abstract trait Strategy {
 }
 
 trait AlwaysCooperate extends Strategy {
-   def decide(h:History) = Cooperate
+   def decide(h: History) = Cooperate
+}
+
+trait AlwaysDefect extends Strategy {
+  def decide(h: History) = Defect
+}
+
+trait TitForTat extends Strategy {
+  def decide(h: History) = h match {
+    case Seq( Round(_, opponentMove), _*) => opponentMove
+    case _ => Cooperate
+  }
+}
+
+trait TitForTwoTats extends Strategy {
+  def decide(h: History) = h match {
+    case Seq( Round(_, Defect), Round(_, Defect), _*) => Defect
+    case _ => Cooperate
+  }
+}
+
+trait CooperateDefect extends Strategy {
+  def decide(h: History) = h.size % 2 match {
+    case 0 => Cooperate
+    case 1 => Defect
+  }
+}
+
+trait CooperateCooperateDefect extends Strategy {
+  def decide(h: History) = h.size % 3 match {
+    case 0 | 1 => Cooperate
+    case 2 => Defect
+  }
+}
+
+trait DefectDefectCooperate extends Strategy {
+  def decide(h: History) = h.size % 3 match {
+    case 0 | 1 => Defect
+    case 2 => Cooperate
+  }
+}
+
+trait Spiteful extends Strategy {
+  def decide(h: History) =
+    if (h.exists( {case(Round(_, hisMove)) => hisMove == Defect} )) Defect
+    else Cooperate
+}
+
+trait Mistrust extends Strategy {
+  def decide(h: History) = h match {
+    case Seq() => Defect
+    case Seq(Round(_, Defect), _*) => Defect
+    case _ => Cooperate
+  }
+}
+
+trait Pavlov extends Strategy {
+  def decide(h: History) = h match {
+    case Seq() => Cooperate
+    case Seq(Round(myMove, opponentMove), _*) if myMove == opponentMove => Cooperate
+    case _ => Defect
+  }
 }
 
 class Player(val name: PlayerId) {
