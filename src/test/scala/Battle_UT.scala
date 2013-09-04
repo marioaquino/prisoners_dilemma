7252f421fefcb7744a4281a2f14c48563e3c5676
Mario Aquino <mario.e.aquino@gmail.com>
2013-09-04 00:46:35 -0500

    Test a battle with all strategies

--- src/test/scala/Battle_UT.scala
+++ src/test/scala/Battle_UT.scala
@@ -85,5 +85,32 @@ class BigFightTestOfDoom extends FunSpec with ShouldMatchers {
 
 
   }
+}
 
+class Holmgang extends FunSpec with ShouldMatchers {
+  import GiantFightOfDoom._
+
+  describe("for evolution!") {
+    implicit val battleConstructor = () => new Battle(100) with OneSetOfRules
+
+    it("picks the evolutionarily stable strategy") {
+      val sucker = new Player("Always Cooperates") with AlwaysCooperate
+      val cheat = new Player("Always Defects") with AlwaysDefect
+      val titForTat =
+        new Player("Starts with cooperation. If defected against, TFT responds with a defect. Otherwise TFT cooperates.") with TitForTat
+      val titForTwoTats =
+        new Player("Starts with cooperate. If defected against twice in a row, TFTT defects; otherwise it cooperates.") with TitForTwoTats
+      val cooperateDefect = new Player("Alternates between cooperate and defect") with CooperateDefect
+      val cooperateCooperateDefect = new Player("Cooperates twice then defects, no matter what.") with CooperateCooperateDefect
+      val defectDefectCooperate = new Player("Defects twice then cooperates, no matter what.") with DefectDefectCooperate
+      val spiteful = new Player("Defects each time after it has been defected against. Initially cooperates.") with Spiteful
+      val mistrust = new Player("Initially defects. Defects if it has been defected against in the last move. Otherwise cooperates.") with Mistrust
+      val pavlov = new Player("Initially cooperates. Cooperates if the last move was the same as the opponents. Otherwise defects.") with Pavlov
+
+      val result = declareAWinner(everybodyFight(Seq(sucker, cheat, titForTat, titForTwoTats, cooperateDefect, cooperateCooperateDefect,
+       defectDefectCooperate, spiteful, mistrust, pavlov)))
+
+      result.head should equal(titForTwoTats)
+    }
+  }
 }
