26508b66839203965474bd3a5d852f2d987f0237
Jessica Kerr <jessitron@gmail.com>
2013-09-04 20:51:54 -0500

    It does something useful and interestinggit add .

--- src/main/scala/TryVariousRules.scala
+++ src/main/scala/TryVariousRules.scala
@@ -3,28 +3,28 @@ package pd
 object TryVariousRules {
 
   /*
-   *  -50                                                 50
-   *   |--------------------------------------------------|
-   *    |         |          |              |
-   *    loneCoop  |          |              |
-   *              bothDefect |              |
-   *                         bothCooperate  |
-   *                                        loneDefector
+   *  -50                                      50
+   *   |---------------------------------------|
+   *       |         |          |              |
+   *       loneCoop  |          |              |
+   *                 bothDefect |              |
+   *                            bothCooperate  |
+   *                                           loneDefector
    */
    def main(args: Array[String]) {
 
       val lowestScore = -5
       val highestScore = 5
-      val n = 5
+      val n = 50
 
       val wiggleRoom = highestScore - lowestScore
 
       val results =
-      for {loneC <- Range(lowestScore, highestScore)
-           bothD <- Range(loneC, highestScore)
-           bothC <- Range(bothD, highestScore)
-           loneD <- Range(bothC, highestScore) } // todo: always use highestScore here
+      for {loneC <- Range(lowestScore, highestScore+1)
+           bothD <- Range(loneC, highestScore+1)
+           bothC <- Range(bothD, highestScore+1) }
       yield {
+        val loneD = highestScore
         val battle = new Battle(n) with Rules {
           val bothCooperate = bothC
           val bothDefect = bothD
@@ -36,7 +36,7 @@ object TryVariousRules {
             val s2 = "-" * (bothDefect - loneCooperator)
             val s3 = "-" * (bothCooperate - bothDefect)
             val s4 = "-" * (loneDefector - bothCooperate)
-            s"$s1*$s2*$s3*$s4*"
+            s"${s1}c${s2}D${s3}C${s4}d"
           }
         }
 
@@ -47,8 +47,15 @@ object TryVariousRules {
         (battle, winner)
       }
 
-      results.foreach { case(battle, winner) => println(s"$battle $winner")}
 
+      results.foreach { case(battle, winner) => println(s"$battle ${printWinner(winner)}")}
+
+   }
+
+   private def printWinner(winners: Seq[Player]):String = winners match {
+     case Seq(one) => one.toString
+     case Seq(one, two) => s"$one & $two tied"
+     case longer => s"${longer.size}-way tie"
    }
 
 }
